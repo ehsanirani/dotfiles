6532cd035ad2f92545a01143a25f664746f5094c
Ehsan Irani <ehsan.irani@protonmail.com>
2025-09-15 22:36:06 +0200

    add dell laptop host module

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Ehsan’s system + user config (NixOS 25.05)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }: let
+    system = "x86_64-linux";
+  in {
+    homeConfigurations.ehsan = home-manager.lib.homeManagerConfiguration {
+      inherit system;
+      modules = [ ./home/ehsan.nix ];
+    };
+
+    nixosConfigurations.laptop-dell = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./hosts/laptop-dell.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.ehsan = import ./home/ehsan.nix;
+        }
+      ];
+    };
+  };
+}
+
---
+++ hosts/laptop-dell.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  # Boot loader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Networking
+  networking.hostName = "laptop-dell";
+  networking.networkmanager.enable = true;
+
+  # Locale & time
+  time.timeZone = "Europe/Berlin"; # change if needed
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Desktop
+  services.xserver = {
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+  };
+
+  # Audio (PipeWire)
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Users
+  users.users.ehsan = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [ ]; # add your SSH keys here
+  };
+
+  # Security
+  security.sudo.wheelNeedsPassword = false;
+
+  # Nix settings
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    auto-optimise-store = true;
+  };
+
+  # System-wide packages
+  environment.systemPackages = with pkgs; [
+    git wget curl gnugrep gnutar gzip
+  ];
+
+  # NEVER change after install
+  system.stateVersion = "25.05";
+}
+
