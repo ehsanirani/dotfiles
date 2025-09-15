1e5c834a426cfed2fea9ebf554b35abbd93d01c0
Ehsan Irani <ehsan.irani@protonmail.com>
2025-09-15 23:03:51 +0200

    use latest emacs & julia

--- home/ehsan.nix
+++ home/ehsan.nix
@@ -1,10 +1,19 @@
 { config, pkgs, lib, ... }:
 
+let
+  ompTheme = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/powerlevel10k_lean_extended.omp.json";
+    sha256 = "";   # `nix-prefetch-url <url>` if you want a fixed hash
+  };
+in
 {
   home.username = "ehsan";
   home.homeDirectory = "/home/ehsan";
   home.stateVersion = "25.05";
 
+  #########################################################################
+  # PROGRAMS  (latest versions by default)
+  #########################################################################
   programs.git = {
     enable = true;
     userName  = "Ehsan Irani";
@@ -19,36 +28,67 @@
     enable = true;
     enableAutosuggestions = true;
     enableSyntaxHighlighting = true;
+
+    # prompt & completions
     initContent = ''
-      export EDITOR=emacsclient
-      eval "$(zoxide init zsh)"
+      eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ${ompTheme})"
+      source <(${pkgs.fzf}/bin/fzf --zsh)
+      eval "$(${pkgs.zoxide}/bin/zoxide init zsh)"
+      eval "$(${pkgs.uv}/bin/uv generate-shell-completion zsh)"
+      eval "$(${pkgs.uv}/bin/uvx --generate-shell-completion zsh)"
+
+      alias ls='${pkgs.eza}/bin/eza --icons --git'
+      alias ll='${pkgs.eza}/bin/eza -l --icons --git'
+      alias cat='${pkgs.bat}/bin/bat'
+      alias find='${pkgs.fd}/bin/fd'
+      alias grep='${pkgs.ripgrep}/bin/rg'
+
+      [[ -d "$HOME/bin"        ]] && PATH="$HOME/bin:$PATH"
+      [[ -d "$HOME/.local/bin" ]] && PATH="$HOME/.local/bin:$PATH"
+
     '';
+
+    sessionVariables = { EDITOR = "emacsclient"; };
   };
 
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    extraConfig = ''
-      set number relativenumber
-    '';
+    extraConfig = "set number relativenumber";
   };
 
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs;  # always latest in nixpkgs
+    # no version pinned → latest stable
   };
 
+  #########################################################################
+  # PACKAGES  (latest by default)
+  #########################################################################
   home.packages = with pkgs; [
-    julia
+    julia          # newest stable
     python3
     uv
     rustc
     cargo
-    ripgrep fd eza tree tmux zellij htop btop
-    zoxide direnv
+    oh-my-posh
+    fzf
+    zoxide
+    eza
+    bat
+    fd
+    ripgrep
+    tree
+    tmux
+    htop
+    btop
     firefox
+    zellij
   ];
 
+  #########################################################################
+  # SERVICES
+  #########################################################################
   systemd.user.services.emacs-daemon = {
     Unit = {
       Description = "Emacs daemon";
@@ -60,8 +100,4 @@
     };
     Install = { WantedBy = [ "default.target" ]; };
   };
-
-  xdg.configFile."alacritty/alacritty.yml".source =
-    ./config/alacritty/alacritty.yml;
 }
-
