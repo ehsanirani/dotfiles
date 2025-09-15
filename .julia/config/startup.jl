395a845aee3c851b32ab4ed2c179126ee2aa5f1e
Ehsan Irani <ehsan.irani@protonmail.com>
2025-09-15 17:29:18 +0200

    update julia startup.jl

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,9 +1,4 @@
+using OhMyREPL
+using BenchmarkTools
 using Revise
 
-atreplinit() do repl
-    try
-        @eval using OhMyREPL
-    catch e
-        @warn "error while importing OhMyREPL" e
-    end
-end
