46709421ebb949de973af4b917b4a72730068d89
Ehsan Irani <ehsan.irani@protonmail.com>
2025-09-16 14:59:56 +0200

    update keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 {
   "id_ed25519.age".publicKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICxqxVL5tXvciZRJEU16w4mDjPw8AOWNWssaCivqW+7r ehsan@laptop-dell"   # your *public* key
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPjIB7ThDqQrolSIzH51CswmqXfB/g7mehkB5LtImy7c ehsan@laptop-dell"
   ];
 }
 
