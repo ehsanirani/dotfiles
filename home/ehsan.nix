b5bd6af0ab7ee5d8b4367dccb0f2837512e9307d
Ehsan Irani <ehsan.irani@protonmail.com>
2025-10-02 22:51:37 +0200

    Fix Nerd Fonts installation for NixOS 25.05 and enable fontconfig

--- home/ehsan.nix
+++ home/ehsan.nix
@@ -102,9 +102,14 @@ in
     btop
     firefox
     zellij
-    # Nerd Fonts for icons
-    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Iosevka" ]; })
+    # Nerd Fonts for icons (new syntax for NixOS 25.05)
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.iosevka
   ];
+
+  # Enable fontconfig for proper font detection
+  fonts.fontconfig.enable = true;
 
   #########################################################################
   # SERVICES
