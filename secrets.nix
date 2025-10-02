b459aa9feaf62fb56d46fcaceec0f4e6cd9885ba
Ehsan Irani <ehsan.irani@protonmail.com>
2025-10-02 16:52:46 +0200

    Update SSH key encryption with host key

---
+++ secrets.nix
@@ -0,0 +1,7 @@
+let
+  user-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNgLKtqZqtHJOpS7TgcVosTOmYON+pvLB4jXF4MViab ehsan@laptop-dell";  # paste from ~/.ssh/id_ed25519_new.pub
+  host-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILo+vz2klZ6lV1SdlgjCtFTkaG0ak1NVrz8svQrFwnKD root@laptop-dell";   # paste from host key above
+in
+{
+  "secrets/id_ed25519.age".publicKeys = [ user-key host-key ];
+}
