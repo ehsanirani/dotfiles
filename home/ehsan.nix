3bbe80b0bb4dcfb7331f5ed4f2a4d59557c199d0
Ehsan Irani <ehsan.irani@protonmail.com>
2025-10-03 10:59:33 +0200

    use z for cd command

--- home/ehsan.nix
+++ home/ehsan.nix
@@ -49,13 +49,13 @@ in
     initContent = ''
       eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ${ompTheme})"
       source <(${pkgs.fzf}/bin/fzf --zsh)
-      eval "$(${pkgs.zoxide}/bin/zoxide init zsh)"
+      eval "$(${pkgs.zoxide}/bin/zoxide init zsh --cmd cd)"
       eval "$(${pkgs.uv}/bin/uv generate-shell-completion zsh)"
       eval "$(${pkgs.uv}/bin/uvx --generate-shell-completion zsh)"
 
       alias ls='${pkgs.eza}/bin/eza --icons --git'
       alias ll='${pkgs.eza}/bin/eza -l --icons --git'
-      alias cat='${pkgs.bat}/bin/bat'
+      # alias cat='${pkgs.bat}/bin/bat'
       alias find='${pkgs.fd}/bin/fd'
       alias grep='${pkgs.ripgrep}/bin/rg'
 
@@ -89,6 +89,7 @@ in
     kitty
     foot
     rio
+    jujutsu
     python3
     uv
     rustc
@@ -128,15 +129,15 @@ in
   #########################################################################
   # SERVICES
   #########################################################################
-  systemd.user.services.emacs-daemon = {
-    Unit = {
-      Description = "Emacs daemon";
-      After = [ "graphical-session.target" ];
-    };
-    Service = {
-      ExecStart = "${pkgs.emacs}/bin/emacs --daemon";
-      ExecStop  = "${pkgs.emacs}/bin/emacsclient -e '(kill-emacs)'";
-    };
-    Install = { WantedBy = [ "default.target" ]; };
-  };
+  #systemd.user.services.emacs-daemon = {
+  #  Unit = {
+  #    Description = "Emacs daemon";
+  #    After = [ "graphical-session.target" ];
+  #  };
+  #  Service = {
+  #    ExecStart = "${pkgs.emacs}/bin/emacs --daemon";
+  #    ExecStop  = "${pkgs.emacs}/bin/emacsclient -e '(kill-emacs)'";
+  #  };
+  #  Install = { WantedBy = [ "default.target" ]; };
+  #};
 }
