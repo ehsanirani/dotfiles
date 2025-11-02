c84f5efb0dea022710dd1141a3f8f628ae64ca95
Ehsan Irani <ehsan.irani@protonmail.com>
2025-11-02 08:06:55 +0100

    add Robto Slab and Lato fonts

--- home/modules/fonts.nix
+++ home/modules/fonts.nix
@@ -6,6 +6,9 @@
     nerd-fonts.fira-code
     nerd-fonts.jetbrains-mono
     nerd-fonts.iosevka
+    # Robot Slab and Lato fonts
+    roboto-slab
+    lato
   ];
 
   # Enable fontconfig for proper font detection
