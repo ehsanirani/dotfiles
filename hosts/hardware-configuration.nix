1396219ae9dc30d2eb1a5a8e7bf9ac1c847b8e62
Ehsan Irani <ehsan.irani@protonmail.com>
2025-11-27 13:09:09 +0100

    enable opengl support

--- hosts/hardware-configuration.nix
+++ hosts/hardware-configuration.nix
@@ -37,4 +37,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Enable opengl
+  hardware.opengl.enable = true;
 }
