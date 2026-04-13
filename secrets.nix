d6d4ada2fa71f96a545a86ac72afea10392edb5c
Ehsan Irani <ehsan.irani@protonmail.com>
2026-04-13 10:29:54 +0200

    add HZI laptop public key

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,7 @@
 let
   user-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNgLKtqZqtHJOpS7TgcVosTOmYON+pvLB4jXF4MViab ehsan@laptop-dell";  # from id_ed25519_new.pub
-  host-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILo+vz2klZ6lV1SdlgjCtFTkaG0ak1NVrz8svQrFwnKD root@laptop-dell";   # paste from host key above
+  host-key-dell = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILo+vz2klZ6lV1SdlgjCtFTkaG0ak1NVrz8svQrFwnKD root@laptop-dell";   # paste from host key above
+  host-key-hzi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMrvrMLVI02nIMTuT+GqZSRghWz0N53VllxSKWj1B2dr root@nixos"; # paste from HZI dell laptop
 in
 {
   "secrets/id_ed25519.age".publicKeys = [ user-key host-key ];
